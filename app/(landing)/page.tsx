deba9b9e7c170c8ffe60dd657a8469fafafd98f9
Artyom Narshinov <narshinovartyom@mail.ru>
2024-07-24 17:52:17 +0400

    x

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,12 +1,39 @@
-import {Button} from '@/components/ui/button';
-import {useToast} from '@/components/ui/use-toast';
+
+import {About} from '@/modules/Landing/About';
+import {Cta} from '@/modules/Landing/Cta';
+import {FAQ} from '@/modules/Landing/FAQ';
+import {Features} from '@/modules/Landing/Features';
+import {Footer} from '@/modules/Landing/Footer';
+import {Hero} from '@/modules/Landing/Hero';
+import {HowItWorks} from '@/modules/Landing/HowItWorks';
+import {Navbar} from '@/modules/Landing/Navbar';
+import {Newsletter} from '@/modules/Landing/Newsletter';
+import {Pricing} from '@/modules/Landing/Pricing';
+import {ScrollToTop} from '@/modules/Landing/ScrollToTop';
+import {Services} from '@/modules/Landing/Services';
+import {Sponsors} from '@/modules/Landing/Sponsors';
+import {Team} from '@/modules/Landing/Team';
+import {Testimonials} from '@/modules/Landing/Testimonials';
 import React from 'react';
 
 export default async function Page() {
   return (
-    <div>
-      asdasd
-      <Button variant='destructive'>adsd</Button>
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+      <Sponsors />
+      <About />
+      <HowItWorks />
+      <Features />
+      <Services />
+      <Cta />
+      <Testimonials />
+      <Team />
+      <Pricing />
+      <Newsletter />
+      <FAQ />
+      <Footer />
+      <ScrollToTop />
+    </>
   );
 }
